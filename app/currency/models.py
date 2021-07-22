2c58e6967863e99d78cff48aafb34818e53cd700
topolian <topolian.anna@gmail.com>
2021-07-22 21:31:14 +0300

    create ContactUs model in currency up

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=50)
+    subject = models.CharField(max_length=120)
+    message = models.TextField()
