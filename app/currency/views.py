5fade2549dca647b1307f4c88e71a90c8071c263
topolian <topolian.anna@gmail.com>
2021-07-27 21:24:26 +0300

    fix bugs and add ContactUs data

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from currency.models import ContactUs
 from django.http import HttpResponse
 
 
 def hello_world(request):
     return HttpResponse("Hello World")
+
+def contact_us(request):
+    contacts = ContactUs.objects.all()
+
+    result = []
+    for contact in contacts:
+        result.append(
+            f"id: {contact.id} name: {contact.email_from}  subject: {contact.subject}  message: {contact.message}</br>"
+        )
+
+    return HttpResponse(str(result))
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from currency.views import contact_us
+from currency.views import hello_world
+
 from django.contrib import admin
 from django.urls import path
-from currency.views import hello_world
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('hello-world/', hello_world)
+    path('hello-world/', hello_world),
+
+    path('contact-us/', contact_us)
 ]
