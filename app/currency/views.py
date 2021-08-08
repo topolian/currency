db6a113156e2f3401963915e588bb23f196703fc
topolian <topolian.anna@gmail.com>
2021-08-08 15:51:23 +0300

    add templates

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,19 +1,44 @@
-from currency.models import ContactUs
+from django.shortcuts import render
+
+from currency.models import ContactUs, Rate
 
 from django.http import HttpResponse
 
 
-def hello_world(request):
-    return HttpResponse("Hello World")
+def index(request):
+    return render(request, 'index.html')
 
 
 def contact_us(request):
     contacts = ContactUs.objects.all()
 
-    result = []
-    for contact in contacts:
-        result.append(
-            f"id: {contact.id} name: {contact.email_from}  subject: {contact.subject}  message: {contact.message}</br>"
-        )
+    # result = []
+    # for contact in contacts:
+    #     result.append(
+    #         f"id: {contact.id} "
+    #         f"name: {contact.email_from}  "
+    #         f"subject: {contact.subject}  "
+    #         f"message: {contact.message}</br>"
+    #     )
 
-    return HttpResponse(str(result))
+    context = {
+        'contact_us': contacts,
+    }
+
+    return render(request, 'contact_us.html', context=context)
+
+
+def rate_list(request):
+    rates = Rate.objects.all()
+    #
+    # result = []
+    # for rate in rates:
+    #     result.append(
+    #         f"Id: {rate.id} Sale: {rate.sale} Buy: {rate.buy} Created: {rate.created} Source: {rate.source} Type: {rate.type}</br>"
+    #     )
+
+    context = {
+        'rate_list': rates,
+    }
+
+    return render(request, 'rate_list.html', context=context)
