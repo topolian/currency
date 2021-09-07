a038c1a54795fb6b674f40e49677423490adc490
topolian <topolian.anna@gmail.com>
2021-09-07 18:42:27 +0300

    applied Class Based Views, reverse_lazy, created urls.py in currency

---
+++ app/currency/urls.py
@@ -0,0 +1,21 @@
+from currency.views import (
+    contact_us,
+    RateCreateView, RateDetailView, RateListView,
+    SourceCreateView, SourceDeleteView, SourceDetailsView,
+    SourceListView, SourceUpdateView
+)
+from django.urls import path
+
+app_name = 'currency'
+
+urlpatterns = [
+    path('contact-us/', contact_us, name='contact-us'),
+    path('rate/list/', RateListView.as_view(), name='rate-list'),
+    path('rate/create/', RateCreateView.as_view(), name='rate-create'),
+    path('rate/details/<int:pk>/', RateDetailView.as_view(), name='rate-details'),
+    path('source/list/', SourceListView.as_view(), name='source-list'),
+    path('source/create/', SourceCreateView.as_view(), name='source-create'),
+    path('source/details/<int:pk>/', SourceDetailsView.as_view(), name='source-details'),
+    path('source/update/<int:pk>/', SourceUpdateView.as_view(), name='source-update'),
+    path('source/delete/<int:pk>/', SourceDeleteView.as_view(), name='source-delete'),
+]
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,37 +1,13 @@
-"""settings URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from currency.views import contact_us
-from currency.views import (
-    index, rate_create, rate_list,
-    source_create, source_delete, source_details, source_list, source_update
-)
-
+import debug_toolbar
 from django.contrib import admin
-from django.urls import path
+from django.views.generic import TemplateView
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
 
-    path('contact-us/', contact_us),
-    path('rate/list/', rate_list),
-    path('rate/create/', rate_create),
-    path('source/list/', source_list),
-    path('source/create/', source_create),
-    path('source/details/<int:source_id>/', source_details),
-    path('source/update/<int:source_id>/', source_update),
-    path('source/delete/<int:source_id>/', source_delete),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+
+    path('currency/', include('currency.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
